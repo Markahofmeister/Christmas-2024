2a539baa7399c8da2059969f5bfba8cfb7fecbfb
markahofmeister <markhof12@gmail.com>
2024-12-19 08:02:43 -0500

    Starting board bringup. There's no main function generated by cube.

--- embedded/Core/Src/main.c
+++ embedded/Core/Src/main.c
@@ -69,6 +69,56 @@ static void MX_TIM3_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
+int main(void)
+{
+
+	 /* USER CODE BEGIN 1 */
+
+	  /* USER CODE END 1 */
+
+	  /* MCU Configuration--------------------------------------------------------*/
+
+	  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+		HAL_Init();
+
+	  /* USER CODE BEGIN Init */
+
+	  /* USER CODE END Init */
+
+	  /* Configure the system clock */
+	  SystemClock_Config();
+
+	  /* USER CODE BEGIN SysInit */
+
+	  /* USER CODE END SysInit */
+
+	  /* Initialize all configured peripherals */
+	  MX_GPIO_Init();
+	  MX_DMA_Init();
+	  MX_I2S1_Init();
+	  MX_SPI2_Init();
+	  MX_TIM1_Init();
+	  MX_TIM3_Init();
+	  MX_TIM14_Init();
+	  /* USER CODE BEGIN 2 */
+
+	  /* USER CODE END 2 */
+
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+
+
+	while (1)
+	{
+	  /* USER CODE END WHILE */
+
+	  /* USER CODE BEGIN 3 */
+	}
+	/* USER CODE END 3 */
+}
+
+
+
 /* USER CODE END 0 */
 
 /**
